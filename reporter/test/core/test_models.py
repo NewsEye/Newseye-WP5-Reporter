13b5aa95e20bc49e3fd1fc6e81306f4930f8d302
Leo Leppänen <leo.leppanen@helsinki.fi>
2019-01-29 15:19:14 +0200

    ENH: Use the unittest library methods for testing instead of raw asserts

--- reporter/test/core/test_models.py
+++ reporter/test/core/test_models.py
@@ -1,123 +1,115 @@
 from unittest import main, TestCase
-from reporter.core.models import Message, Fact, DocumentPlanNode, Document, Relation
+from reporter.core.models import Message, Fact, DocumentPlanNode, Document, Relation, TemplateComponent, Slot, \
+    LiteralSource, SlotSource, LiteralSlot, FactFieldSource
 
 
 class TestFact(TestCase):
+
+    def setUp(self):
+        self.fact = Fact('corpus', 'corpus_type', 'timestamp_from', 'timestamp_to', 'timestamp_type', 'analysis_type',
+                         'result_key', 'result_value', 'outlierness')
+
     def test_fact_fields(self):
-        f = Fact('corpus', 'corpus_type', 'timestamp_from', 'timestamp_to', 'timestamp_type', 'analysis_type',
-                 'result_key', 'result_value', 'outlierness')
-        assert f.corpus == 'corpus'
-        assert f.corpus_type == 'corpus_type'
-        assert f.timestamp_from == 'timestamp_from'
-        assert f.timestamp_to == 'timestamp_to'
-        assert f.timestamp_type == 'timestamp_type'
-        assert f.analysis_type == 'analysis_type'
-        assert f.result_key == 'result_key'
-        assert f.result_value == 'result_value'
-        assert f.outlierness == 'outlierness'
+        self.assertEqual(self.fact.corpus, 'corpus')
+        self.assertEqual(self.fact.corpus_type, 'corpus_type')
+        self.assertEqual(self.fact.timestamp_from, 'timestamp_from')
+        self.assertEqual(self.fact.timestamp_to, 'timestamp_to')
+        self.assertEqual(self.fact.timestamp_type, 'timestamp_type')
+        self.assertEqual(self.fact.analysis_type, 'analysis_type')
+        self.assertEqual(self.fact.result_key, 'result_key')
+        self.assertEqual(self.fact.result_value, 'result_value')
+        self.assertEqual(self.fact.outlierness, 'outlierness')
 
 
 class TestMessage(TestCase):
 
-    def test_message_creation_single_fact(self):
-        f = Fact('corpus', 'corpus_type', 'timestamp_from', 'timestamp_to', 'timestamp_type', 'analysis_type',
-                 'result_key', 'result_value', 'outlierness')
-        m = Message(f, 0.1, 0.2, 0.3)
+    def setUp(self):
+        self.fact1 = Fact('corpus1', 'corpus_type', 'timestamp_from', 'timestamp_to', 'timestamp_type', 'analysis_type',
+                          'result_key', 'result_value', 'outlierness')
+        self.fact2 = Fact('corpus2', 'corpus_type', 'timestamp_from', 'timestamp_to', 'timestamp_type', 'analysis_type',
+                          'result_key', 'result_value', 'outlierness')
 
-        assert isinstance(m.facts, list) and not isinstance(m.facts, Fact)
-        assert len(m.facts) == 1
-        assert m.facts[0] == f
-        assert m.main_fact == f
-        assert m.importance_coefficient == 0.1
-        assert m.score == 0.2
-        assert m.polarity == 0.3
-        assert m.template is None
-        assert str(m) == "<Message>"
+    def test_message_creation_single_fact(self):
+        message = Message(self.fact1, 0.1, 0.2, 0.3)
+
+        self.assertIsInstance(message.facts, list)
+        self.assertNotIsInstance(message.facts, Fact)
+        self.assertEqual(len(message.facts), 1)
+        self.assertEqual(message.facts[0], self.fact1)
+        self.assertEqual(message.main_fact, self.fact1)
+        self.assertEqual(message.importance_coefficient, 0.1)
+        self.assertEqual(message.score, 0.2)
+        self.assertEqual(message.polarity, 0.3)
+        self.assertIsNone(message.template)
+        self.assertEqual(str(message), "<Message>")
 
     def test_message_creation_list_of_facts(self):
-        f1 = Fact('corpus1', 'corpus_type', 'timestamp_from', 'timestamp_to', 'timestamp_type', 'analysis_type',
-                  'result_key', 'result_value', 'outlierness')
-        f2 = Fact('corpus2', 'corpus_type', 'timestamp_from', 'timestamp_to', 'timestamp_type', 'analysis_type',
-                  'result_key', 'result_value', 'outlierness')
-        m = Message([f1, f2], 0.1, 0.2, 0.3)
+        message = Message([self.fact1, self.fact2], 0.1, 0.2, 0.3)
 
-        assert isinstance(m.facts, list)
-        assert len(m.facts) == 2
-        assert m.facts == [f1, f2]
-        assert m.main_fact == f1
+        self.assertIsInstance(message.facts, list)
+        self.assertEqual(len(message.facts), 2)
+        self.assertListEqual(message.facts, [self.fact1, self.fact2])
+        self.assertEqual(message.main_fact, self.fact1)
 
     def test_message_set_facts_always_list1(self):
-        f1 = Fact('corpus1', 'corpus_type', 'timestamp_from', 'timestamp_to', 'timestamp_type', 'analysis_type',
-                  'result_key', 'result_value', 'outlierness')
-        f2 = Fact('corpus2', 'corpus_type', 'timestamp_from', 'timestamp_to', 'timestamp_type', 'analysis_type',
-                  'result_key', 'result_value', 'outlierness')
-        m = Message(f1, 0.1, 0.2, 0.3)
+        message = Message(self.fact1, 0.1, 0.2, 0.3)
+        message.facts = self.fact2
 
-        assert m.facts == [f1]
-        assert m.main_fact == f1
-
-        m.facts = f2
-
-        assert m.facts == [f2]
-        assert m.main_fact == f2
+        self.assertIsInstance(message.facts, list)
+        self.assertListEqual(message.facts, [self.fact2])
+        self.assertEqual(message.main_fact, self.fact2)
 
     def test_message_set_facts_always_list2(self):
-        f1 = Fact('corpus1', 'corpus_type', 'timestamp_from', 'timestamp_to', 'timestamp_type', 'analysis_type',
-                  'result_key', 'result_value', 'outlierness')
-        f2 = Fact('corpus2', 'corpus_type', 'timestamp_from', 'timestamp_to', 'timestamp_type', 'analysis_type',
-                  'result_key', 'result_value', 'outlierness')
-        m = Message(f2, 0.1, 0.2, 0.3)
+        message = Message(self.fact1, 0.1, 0.2, 0.3)
+        message.facts = [self.fact2]
 
-        assert m.facts == [f2]
-        assert m.main_fact == f2
-
-        m.facts = [f1, f2]
-
-        assert m.facts == [f1, f2]
-        assert m.main_fact == f1
+        self.assertIsInstance(message.facts, list)
+        self.assertListEqual(message.facts, [self.fact2])
+        self.assertEqual(message.main_fact, self.fact2)
 
 
 class TestDocument(TestCase):
 
-    def test_document(self):
-        f1 = Fact('corpus1', 'corpus_type', 'timestamp_from', 'timestamp_to', 'timestamp_type', 'analysis_type',
-                  'result_key', 'result_value', 'outlierness')
-        m1 = Message(f1, 0.1, 0.2, 0.3)
+    def setUp(self):
+        self.fact1 = Fact('corpus1', 'corpus_type', 'timestamp_from', 'timestamp_to', 'timestamp_type', 'analysis_type',
+                          'result_key', 'result_value', 'outlierness')
+        self.message1 = Message(self.fact1, 0.1, 0.2, 0.3)
 
-        f2 = Fact('corpus2', 'corpus_type', 'timestamp_from', 'timestamp_to', 'timestamp_type', 'analysis_type',
-                  'result_key', 'result_value', 'outlierness')
-        m2 = Message(f2, 0.1, 0.2, 0.3)
+        self.fact2 = Fact('corpus2', 'corpus_type', 'timestamp_from', 'timestamp_to', 'timestamp_type', 'analysis_type',
+                          'result_key', 'result_value', 'outlierness')
+        self.message2 = Message(self.fact2, 0.1, 0.2, 0.3)
+        self.document_plan_node = DocumentPlanNode([self.message1, self.message2], Relation.ELABORATION)
+        self.document = Document('en', self.document_plan_node)
 
-        dp = DocumentPlanNode([m1, m2])
+    def test_document_creation(self):
+        self.assertEqual(self.document.language, 'en')
+        self.assertEqual(self.document.document_plan, self.document_plan_node)
 
-        d = Document('en', dp)
-
-        assert d.language == 'en'
-        assert d.document_plan == dp
-        assert m1 in d.messages()
-        assert m2 in d.messages()
+    def test_document_messages_retrieves_all_messages(self):
+        self.assertIn(self.message1, self.document.messages())
+        self.assertIn(self.message2, self.document.messages())
 
 
 class TestDocumentPlanNode(TestCase):
 
-    def test_document_plan_node_creation(self):
-        f1 = Fact('corpus1', 'corpus_type', 'timestamp_from', 'timestamp_to', 'timestamp_type', 'analysis_type',
-                  'result_key', 'result_value', 'outlierness')
-        m1 = Message(f1, 0.1, 0.2, 0.3)
+    def setUp(self):
+        self.fact1 = Fact('corpus1', 'corpus_type', 'timestamp_from', 'timestamp_to', 'timestamp_type', 'analysis_type',
+                          'result_key', 'result_value', 'outlierness')
+        self.message1 = Message(self.fact1, 0.1, 0.2, 0.3)
 
-        f2 = Fact('corpus2', 'corpus_type', 'timestamp_from', 'timestamp_to', 'timestamp_type', 'analysis_type',
-                  'result_key', 'result_value', 'outlierness')
-        m2 = Message(f2, 0.1, 0.2, 0.3)
+        self.fact2 = Fact('corpus2', 'corpus_type', 'timestamp_from', 'timestamp_to', 'timestamp_type', 'analysis_type',
+                          'result_key', 'result_value', 'outlierness')
+        self.message2 = Message(self.fact2, 0.1, 0.2, 0.3)
 
-        dp = DocumentPlanNode([m1, m2], Relation.ELABORATION)
+        self.document_plan_node = DocumentPlanNode([self.message1, self.message2], Relation.ELABORATION)
 
-        assert dp.children == [m1, m2]
-        assert dp.relation == Relation.ELABORATION
-        assert str(dp) == 'ELABORATION'
-        try:
-            dp.print_tree()
-        except Exception:
-            self.fail('print_tree() failed to run without an exception')
+    def test_document_plan_node_creation_sets_values(self):
+        self.assertListEqual(self.document_plan_node.children, [self.message1, self.message2])
+        self.assertEqual(self.document_plan_node.relation, Relation.ELABORATION)
+        self.assertEqual(str(self.document_plan_node), 'ELABORATION')
+
+    def test_document_plan_node_print_tree_does_not_crash(self):
+        self.document_plan_node.print_tree()
 
 if __name__ == '__main__':
     main()
