b8c5e6c79560695ed3552a3ab6cf30f1e105cbad
Leo Leppänen <leo.leppanen@helsinki.fi>
2019-05-01 13:41:38 +0300

    Message generation support for most of the mock data

--- reporter/newspaper_message_generator.py
+++ reporter/newspaper_message_generator.py
@@ -14,11 +14,11 @@ class NewspaperMessageGenerator(NLGPipelineComponent):
     def __init__(self) -> None:
         self.MESSAGE_GENERATORS = {
             'facet_counts': self._generate_messages_from_facet_counts,
-            'common_topics': self._ignore,
+            'common_topics': self._generate_messages_from_common_topics,
             'topic_analysis': self._ignore
         }
 
-    def run(self, registry: Registry, random: Random, language: str) -> List[Message]:
+    def run(self, registry: Registry, random: Random, language: str) -> Tuple[List[Message]]:
         """
         Run this pipeline component.
         """
@@ -58,9 +58,39 @@ class NewspaperMessageGenerator(NLGPipelineComponent):
         )
         return []
 
-    def _generate_messages_from_facet_counts(self, analysis: 'Analysis'):
+    def _generate_messages_from_common_topics(self, analysis: 'Analysis') -> List[Message]:
         messages = []
         print(analysis.result)
+        corpus_description = []
+        for key, value in analysis.query.values.items():
+            corpus_description.append("[{}:{}]".format(key, value))
+        corpus_description = " ".join(corpus_description)
+        for (topic, count) in analysis.result:
+            messages.append(
+                Message(
+                    # TODO: This needs to be a list for the thing not to crash despite efforts to allow non-lists, see Message
+                    [Fact(
+                        corpus_description,  # corpus
+                        'query_result',  # corpus_type'
+                        None,  # timestamp_from
+                        None,  # timestamp_to
+                        'all_time',  # timestamp_type
+                        'topic_count',  # analysis_type
+                        "[TOPIC:{}]".format(topic),  # result_key
+                        count,  # result_value
+                        Random().randint(5,15),  # outlierness
+                    )]
+                )
+            )
+        return messages
+
+    def _generate_messages_from_facet_counts(self, analysis: 'Analysis') -> List[Message]:
+        messages = []
+        print(analysis.result)
+        corpus_description = []
+        for key, value in analysis.query.values.items():
+            corpus_description.append("[{}:{}]".format(key, value))
+        corpus_description = " ".join(corpus_description)
         for facet in analysis.result:
             for entry in analysis.result[facet]:
                 assert len(entry) == 2 # These are really 2-tuples of (str, int), even if they are in the JSON as lists
@@ -69,15 +99,15 @@ class NewspaperMessageGenerator(NLGPipelineComponent):
                     Message(
                         # TODO: This needs to be a list for the thing not to crash despite efforts to allow non-lists, see Message
                         [Fact(
-                            'test corpus', #corpus
-                            'corpus', #corpus_type'
+                            corpus_description, #corpus
+                            'query_result', #corpus_type'
                             None, #timestamp_from
                             None, #timestamp_to
                             'all_time', #timestamp_type
-                            'count', #analysis_type
+                            'facet_count', #analysis_type
                             "[{}:{}]".format(facet, key), #result_key
                             count, #result_value
-                            1, #outlierness
+                            Random().randint(0,10), #outlierness
                         )]
                     )
                 )
