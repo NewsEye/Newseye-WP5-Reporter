NewsEye/Newseye-WP5-Reporter

commit 1ee5eb56ee8c65d377108f2d8e8a0fbaa01c80b0

Support for TM-based linking in English
This preliminary implementation only works for English
and has only been tested on a single example query. Bugs
are not only possible but even likely.

reporter/newspaper_slot_realizers.py
@@ -139,9 +139,12 @@ class EnglishCollectionNameRealizer(RegexRealizer):
 
 class EnglishNewspaperNameRealizer(RegexRealizer):
     def __init__(self, registry):
-        super().__init__(
-            registry, "en", r"\[NEWSPAPER_NAME:([^\]]+)\]", 1, "published in [ENTITY:NEWSPAPER:{}]"
-        )
+        super().__init__(registry, "en", r"\[NEWSPAPER_NAME:([^\]]+)\]", 1, "published in {}")
+
+
+class EnglishDocumentIdRealizer(RegexRealizer):
+    def __init__(self, registry):
+        super().__init__(registry, "en", r"\[DOCUMENT_ID:([^\]]+)\]", 1, "document with ID {}")
 
 
 class EnglishYearRealizer(RegexRealizer):

reporter/newspaper_message_generator.py
@@ -22,6 +22,7 @@ class NewspaperMessageGenerator(NLGPipelineComponent):
             self._extract_facets_message_generator,
             self._query_topic_model_topic_weights_message_generator,
             self._compute_tf_idf_message_generator,
+            self._tm_document_linking_message_generator,
         ]
 
     def run(
@@ -261,6 +262,40 @@ class NewspaperMessageGenerator(NLGPipelineComponent):
                         ]
                     )
                 )
+        return messages
+
+    def _tm_document_linking_message_generator(
+        self, analysis: "TaskResult", other_analyses: List["TaskResult"]
+    ) -> List[Message]:
+        if analysis.task_parameters.get("utility") != "tm_document_linking":
+            return []
+
+        corpus, corpus_type = self._build_corpus_fields(analysis, other_analyses)
+
+        interestingness_values = analysis.task_result["interestingness"]
+        distances = analysis.task_result["result"]["distance"]
+        similar_docs = analysis.task_result["result"]["similar_docs"]
+
+        messages = []
+        for distance, doc, interestingess in zip(distances, similar_docs, interestingness_values):
+            messages.append(
+                Message(
+                    # TODO: This needs to be a list for the thing not to crash despite efforts to allow non-lists, see Message
+                    [
+                        Fact(
+                            corpus,  # corpus
+                            corpus_type,  # corpus_type'
+                            None,  # timestamp_from
+                            None,  # timestamp_to
+                            "all_time",  # timestamp_type
+                            "tm_document_distance",  # analysis_type
+                            "[DOCUMENT_ID:{}]".format(doc),  # result_key
+                            distance,  # result_value
+                            interestingess,  # outlierness
+                        )
+                    ]
+                )
+            )
         return messages
 
     def _get_parent(

reporter/newspaper_nlg_service.py
@@ -47,6 +47,7 @@ from reporter.newspaper_slot_realizers import (
     EnglishQueryMmFilterRealizer,
     EnglishQueryMmRealizer,
     EnglishQueryFilterRealizer,
+    EnglishDocumentIdRealizer,
 )
 
 from reporter.newspaper_slot_realizers import (
@@ -148,6 +149,7 @@ class NewspaperNlgService(object):
                 EnglishQueryMmFilterRealizer(self.registry),
                 EnglishQueryMmRealizer(self.registry),
                 EnglishQueryFilterRealizer(self.registry),
+                EnglishDocumentIdRealizer(self.registry),
                 # Finnish
                 FinnishFormatRealizer(self.registry),
                 FinnishLanguageRealizer(self.registry),

commit 6858b6faaca8464efb0996cd2695a728df8166a9

FIX: Newspaper name not properly realized
This undoes a bug that was introduced in commit 1ee5eb56
wherein the newspaper names were no not always properly
realized.

reporter/newspaper_slot_realizers.py
@@ -139,7 +139,7 @@ class EnglishCollectionNameRealizer(RegexRealizer):
 
 class EnglishNewspaperNameRealizer(RegexRealizer):
     def __init__(self, registry):
-        super().__init__(registry, "en", r"\[NEWSPAPER_NAME:([^\]]+)\]", 1, "published in {}")
+        super().__init__(registry, "en", r"\[NEWSPAPER_NAME:([^\]]+)\]", 1, "published in [ENTITY:NEWSPAPER:{}]")
 
 
 class EnglishDocumentIdRealizer(RegexRealizer):