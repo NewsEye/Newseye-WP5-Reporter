f0505e0a64522f5e453a526bedeb0dc5832b47eb
Leo Leppänen <leo.leppanen@helsinki.fi>
2019-10-15 11:10:21 +0300

    MINOR: Mark Mather._equal_op as static
    This just makes explicit the existing semantics, as the method didn't have
    a self argument in the first place.

--- reporter/core/models.py
+++ reporter/core/models.py
@@ -589,6 +589,7 @@ class ReferentialExpr(object):
 
 
 class Matcher(object):
+    @staticmethod
     def _equal_op(a: Any, b: Any) -> bool:
         if type(b) is str:
             return re.match("^" + b + "$", str(a)) is not None
