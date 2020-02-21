edb6752e0063bf7d8ec22a2a41400fde72ad637e
Leo Leppänen <leo.leppanen@helsinki.fi>
2020-02-21 10:38:11 +0200

    Add tests for Finnish morphological realizer

---
+++ test/test_finnish_uralicNLP_morphological_realizer.py
@@ -0,0 +1,41 @@
+from unittest import TestCase, main
+
+from reporter.core.models import Fact, FactField, FactFieldSource, LiteralSlot, Matcher, Message, Slot, Template
+from reporter.finnish_uralicNLP_morphological_realizer import FinnishUralicNLPMorphologicalRealizer
+
+
+class TestRealization(TestCase):
+    def setUp(self):
+        self.fact = Fact("1", "_", "_", "_", "_", "_", "_", "kissa", "_")
+        self.message = Message(self.fact)
+
+        self.expr = FactField("corpus")
+        self.matcher = Matcher(self.expr, "=", "1")
+        self.rules = [([self.matcher], [0])]
+
+        self.slot = Slot(FactFieldSource("result_value"))
+        self.literal = LiteralSlot("sana")
+        self.components = [self.slot, self.literal]
+
+        self.template = Template(self.components, self.rules)
+        self.template.fill(self.message, [self.message])
+
+        self.realizer = FinnishUralicNLPMorphologicalRealizer()
+
+    def test_no_attrs_slot_left_as_is(self):
+        self.assertEqual("kissa", self.realizer.realize(self.slot))
+
+    def test_no_attrs_literal_left_as_is(self):
+        self.assertEqual("sana", self.realizer.realize(self.literal))
+
+    def test_gen_slot_realized_correctly(self):
+        self.slot.attributes["case"] = "genitive"
+        self.assertEqual("kissan", self.realizer.realize(self.slot))
+
+    def test_gen_literal_realized_correctly(self):
+        self.literal.attributes["case"] = "genitive"
+        self.assertEqual("sanan", self.realizer.realize(self.literal))
+
+
+if __name__ == "__main__":
+    main()
